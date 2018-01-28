2b1c6e9be1217da6dbbcb4cd1dfcaf90898b6481
stevenobi <s.obermeyer@t-online.de>
2018-01-28 19:26:00 +0100

    added

---
+++ lib/apxusr-rest.sql
@@ -0,0 +1,84 @@
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- ORACLE Application Express (APEX) export file
+--
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_050100 or as the owner (parsing schema) of the application.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_api.import_begin (
+ p_version_yyyy_mm_dd=>'2016.08.24'
+,p_default_workspace_id=>1831840297345167
+);
+end;
+/
+begin
+wwv_flow_api.remove_restful_service(
+ p_id=>wwv_flow_api.id(6001454321700526)
+,p_name=>'apxusr'
+);
+ 
+end;
+/
+prompt --application/restful_services/apxusr
+begin
+wwv_flow_api.create_restful_module(
+ p_id=>wwv_flow_api.id(6001454321700526)
+,p_name=>'apxusr'
+,p_uri_prefix=>'apxusr/'
+,p_parsing_schema=>'APXDBA'
+,p_items_per_page=>25
+,p_status=>'PUBLISHED'
+,p_row_version_number=>50
+);
+wwv_flow_api.create_restful_template(
+ p_id=>wwv_flow_api.id(6001546744700527)
+,p_module_id=>wwv_flow_api.id(6001454321700526)
+,p_uri_template=>'usr/{usrname}'
+,p_priority=>0
+,p_etag_type=>'HASH'
+);
+wwv_flow_api.create_restful_handler(
+ p_id=>wwv_flow_api.id(6001624143700527)
+,p_template_id=>wwv_flow_api.id(6001546744700527)
+,p_source_type=>'QUERY_1_ROW'
+,p_format=>'DEFAULT'
+,p_method=>'GET'
+,p_require_https=>'NO'
+,p_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select user_status + valid_domain as user_status',
+'from (',
+'    select case when user_exists = 0',
+'                then 0',
+'                else user_status',
+'           end as user_status,',
+'           case when user_exists = 0',
+'                -- when p_return_as_offset is TRUE and without any more args to is_valid_domain, ',
+'                -- the offset is determined by the system setting for ENFORCE_VALID_DOMAIN in APX$CFG',
+'                -- if you specify an offset you achieve better performance, since that additional lookup is avoided.',
+'                then apxusr.is_valid_domain(upper(trim(:USRNAME)), p_return_as_offset => ''TRUE'', p_return_offset => -1)',
+'                else 0',
+'           end as valid_domain',
+'    from',
+'    (select count(1)            as user_exists,',
+'            max(user_status)    as user_status',
+'     from "APXUSR"."APEX_USER_REG_STATUS"',
+'     where upper(trim(username)) = upper(trim(:USRNAME))',
+'    )',
+' )',
+''))
+);
+end;
+/
+begin
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
